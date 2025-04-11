534e5e8a536ef6ba7f306e7db4d2aa0e695ee830
kait <keightbillion@gmail.com>
2025-04-11 10:24:47 -0400

    did stuff idk

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import math
 
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, send_file
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, FileField
 from wtforms.validators import DataRequired, Optional
@@ -13,6 +13,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '7f4bc5403ecbc99f2b10dc1c582be3f9632369dea8d6e45d'
 
 boards = ['b', 'tomo', 'nottomo']
+sql = "INSERT INTO posts(post_id, board_id, thread_id, op, last_bump, time, name, email, subject, content, filename, spoiler) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
 """
 con = sqlite3.connect("tomochan.db")
@@ -20,8 +21,10 @@ cur = con.cursor()
 
 cur.execute("CREATE TABLE IF NOT EXISTS posts("
                 "post_id INTEGER PRIMARY KEY UNIQUE NOT NULL, "
-                "board_id INTEGER NOT NULL, "
+                "board_id TEXT NOT NULL, "
                 "thread_id INTEGER NOT NULL, "
+                "op INTEGER NOT NULL, "
+                "last_bump INTEGER, "
                 "time INTEGER NOT NULL, "
                 "name TEXT NOT NULL, "
                 "email TEXT, "
@@ -29,33 +32,80 @@ cur.execute("CREATE TABLE IF NOT EXISTS posts("
                 "content TEXT NOT NULL, "
                 "filename TEXT, "
                 "spoiler INTEGER NOT NULL)")
+                
+
+insert into posts (post_id, board_id, thread_id, op, last_bump, time, name, email, subject, content, filename, spoiler) values(1, 'b', 1, 1, 1744376519, 1744374813, 'Anonymous', NULL, 'wow', 'i am inserting this with sqlite cmd', NULL, 0);
+insert into posts (post_id, board_id, thread_id, op, last_bump, time, name, email, subject, content, filename, spoiler) values(2, 'b', 2, 1, 1744375966, 1744375966, 'Anonymous', NULL, 'wow again', 'second test post', NULL, 0);
+insert into posts (post_id, board_id, thread_id, op, last_bump, time, name, email, subject, content, filename, spoiler) values(3, 'b', 1, 0, NULL, 1744376519, 'tomo', NULL, NULL, 'i too am in this thread', NULL, 0);
+
 """
 
 def post(new_post):
     con = sqlite3.connect("tomochan.db")
     cur = con.cursor()
-    largest_post_id = cur.execute("SELECT max(post_id)")
+    largest_post_id = cur.execute('SELECT max(post_id) from posts').fetchone()[0]
     if not largest_post_id:
         post_id = 1
     else:
-        post_id = largest_post_id.fetchone()[0]
-
-    
-    cur.execute("INSERT INTO post ")
+        post_id = largest_post_id + 1
+    if new_post['thread_id'] == 'new':
+        largest_thread_id = cur.execute('SELECT max(thread_id) from posts').fetchone()[0]
+        if not largest_thread_id:
+            thread_id = 1
+        else:
+            thread_id = largest_thread_id + 1
+    else:
+        thread_id = new_post['thread_id']
+    if new_post['email'] == 'sage':
+        cur.execute(sql,
+                    (post_id,
+                     new_post['board_id'],
+                     thread_id,
+                     new_post['op'],
+                     None,
+                     new_post['time'],
+                     new_post['name'],
+                     new_post['email'],
+                     new_post['subject'],
+                     new_post['content'],
+                     new_post['filename'],
+                     new_post['spoiler']))
+    else:
+        cur.execute(sql,
+                    (post_id,
+                     new_post['board_id'],
+                     thread_id,
+                     new_post['op'],
+                     new_post['last_bump'],
+                     new_post['time'],
+                     new_post['name'],
+                     new_post['email'],
+                     new_post['subject'],
+                     new_post['content'],
+                     new_post['filename'],
+                     new_post['spoiler']))
+    con.commit()
 
 def get_banner(board):
     banner = random.choice(os.listdir("static/banners"))
     return "/static/banners/" + banner
 
-def get_swatch(time):
-    bst = time + timedelta(hours=1)
+def get_swatch(timestamp):
+    time = datetime.fromtimestamp(timestamp)
+    bmt = time + timedelta(hours=1)
     beat = math.floor(
-        bst.hour * 41.666 +
-        bst.minute * 0.6944 +
-        bst.second * 0.011574
+        bmt.hour * 41.666 +
+        bmt.minute * 0.6944 +
+        bmt.second * 0.011574
     ) % 1000
     return beat
 
+def get_strftime(timestamp):
+    time = datetime.fromtimestamp(timestamp)
+    return time.strftime("%Y-%m-%d(%a) %H:%M:%S")
+
+
+app.jinja_env.globals.update(get_swatch=get_swatch, get_strftime=get_strftime)
 
 class PostForm(FlaskForm):
     name = StringField('Name', validators=[Optional()])
@@ -64,31 +114,20 @@ class PostForm(FlaskForm):
     post = TextAreaField('Content', validators=[DataRequired()])
     file = FileField('File', validators=[Optional()])
     spoiler = BooleanField('Spoiler?', validators=[Optional()])
-    submit = SubmitField('post')
+    submit = SubmitField('Post')
 
 @app.route("/<board>/", methods=['GET', 'POST'])
 def board_page(board):
     if board in boards:
         con = sqlite3.connect("tomochan.db")
         cur = con.cursor()
-        threads = cur.execute("SELECT thread_id, min(post_id) FROM posts")
+        threads = cur.execute("SELECT * FROM posts WHERE op = 1 AND board_id = '" + board + "' ORDER BY last_bump DESC")
         threadlist = threads.fetchmany(size=15)
+        print(threadlist)
 
         banner = get_banner(board)
 
         form = PostForm()
-
-        """
-        if form.validate_on_submit():
-            postcontent = form.post.data.split('\n')
-            #postcontent = form.post.data.replace('\n', '<br>')
-            post_time = datetime.now(timezone.utc)
-            timestamp = post_time.strftime("%Y-%m-%d %H:%M:%S")
-            swatch = get_swatch(post_time)
-            id = len(posts) + 1
-            new_post = { 'name' : name, 'subject' : form.subject.data, 'content' : postcontent, 'time' : post_time, 'timestamp' : timestamp, 'swatch' : swatch, 'id' : id }
-            post(board, posts, new_post)
-        """
         if form.validate_on_submit():
             if form.name.data:
                 name = form.name.data
@@ -96,14 +135,23 @@ def board_page(board):
                 name = "Anonymous"
             email = form.email.data
             subject = form.subject.data
-            content = form.post.data.split('\n')
             spoiler = form.spoiler.data
-            timestamp = datetime.now(timezone.utc).timestamp()
-            new_post = {'name' : name, 'email' : email, 'subject' : subject, 'content' : content, 'spoiler' : spoiler, 'timestamp' : timestamp}
-            post(board, new_post)
+            timestamp = last_bump = datetime.now(timezone.utc).timestamp()
+            new_post = {'thread_id' : 'new',
+                        'board_id' : board,
+                        'op' : 1,
+                        'last_bump' : last_bump,
+                        'name' : name,
+                        'email' : email,
+                        'subject' : subject,
+                        'content' : form.post.data,
+                        'spoiler' : spoiler,
+                        'time' : timestamp,
+                        'filename' : None}
+            post(new_post)
         return render_template('board.html', board=board, form=form, threads=threadlist, banner=banner)
     else:
-        return "not a real board lol", 404
+        return send_file('static/404.html'), 404
 
 """
 @app.route("/<board>/<thread>/", methods=['GET', 'POST'])
