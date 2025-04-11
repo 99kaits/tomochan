30b7f49f1e8218597d7decbf1029e74a1bec1fd1
kait <keightbillion@gmail.com>
2025-04-11 13:20:14 -0400

    did a bunch of stuff again

--- README.md
+++ README.md
@@ -4,15 +4,24 @@ some type of imageboard idk
 or well eventually images
 
 TODO:
-- threads
-- replies
-- post ids
+- file uploads
 - aesthetics
-- something better for timestamps
-- swatch internet time
-- sql db of some type
+- board page showing last 5 threads
+- maybe some other email easter eggs besides sage and nonoko?
+- sage and nonoko, gotta add that but idk what the thing to combine them is because im at the library and i dont want to open 4chan at the library right now
+- reply button on threads on the board view too
+- other pages, maybe an /ukko/ or homepage or rules/faq or whatever
+- funny 404 pages
+- board specific banners
 
 DONE:
 - posting
 - displaying posts
-- saving posts (using pickle)
+- saving posts (NOT using pickle im using sqlite now)
+- something better for timestamps
+- swatch internet time
+- sql db of some type
+- threads
+- replies
+- post ids
+- sage and nonoko (im going for modern 4ch behavior there its just actually better defaulting to noko)
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import math
 
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, FileField
 from wtforms.validators import DataRequired, Optional
@@ -13,9 +13,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '7f4bc5403ecbc99f2b10dc1c582be3f9632369dea8d6e45d'
 
 boards = ['b', 'tomo', 'nottomo']
-sql = "INSERT INTO posts(post_id, board_id, thread_id, op, last_bump, time, name, email, subject, content, filename, spoiler) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+sql = ("INSERT INTO posts(post_id, board_id, thread_id, op, last_bump, time, name, email, subject, content, filename, spoiler) "
+       "values(:post_id, :board_id, :thread_id, :op, :last_bump, :time, :name, :email, :subject, :content, :filename, :spoiler)")
 
-"""
+
 con = sqlite3.connect("tomochan.db")
 cur = con.cursor()
 
@@ -32,59 +33,40 @@ cur.execute("CREATE TABLE IF NOT EXISTS posts("
                 "content TEXT NOT NULL, "
                 "filename TEXT, "
                 "spoiler INTEGER NOT NULL)")
-                
 
+con.close()
+
+"""
 insert into posts (post_id, board_id, thread_id, op, last_bump, time, name, email, subject, content, filename, spoiler) values(1, 'b', 1, 1, 1744376519, 1744374813, 'Anonymous', NULL, 'wow', 'i am inserting this with sqlite cmd', NULL, 0);
 insert into posts (post_id, board_id, thread_id, op, last_bump, time, name, email, subject, content, filename, spoiler) values(2, 'b', 2, 1, 1744375966, 1744375966, 'Anonymous', NULL, 'wow again', 'second test post', NULL, 0);
 insert into posts (post_id, board_id, thread_id, op, last_bump, time, name, email, subject, content, filename, spoiler) values(3, 'b', 1, 0, NULL, 1744376519, 'tomo', NULL, NULL, 'i too am in this thread', NULL, 0);
 
 """
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 def post(new_post):
     con = sqlite3.connect("tomochan.db")
     cur = con.cursor()
     largest_post_id = cur.execute('SELECT max(post_id) from posts').fetchone()[0]
     if not largest_post_id:
-        post_id = 1
+        new_post['post_id'] = 1
     else:
-        post_id = largest_post_id + 1
+        new_post['post_id'] = largest_post_id + 1
     if new_post['thread_id'] == 'new':
-        largest_thread_id = cur.execute('SELECT max(thread_id) from posts').fetchone()[0]
-        if not largest_thread_id:
-            thread_id = 1
-        else:
-            thread_id = largest_thread_id + 1
+        new_post['thread_id'] = new_post['post_id']
+    if new_post['email'] == 'sage' and new_post['op'] == 0:
+        new_post['last_bump'] = None
+        cur.execute(sql, new_post)
     else:
-        thread_id = new_post['thread_id']
-    if new_post['email'] == 'sage':
-        cur.execute(sql,
-                    (post_id,
-                     new_post['board_id'],
-                     thread_id,
-                     new_post['op'],
-                     None,
-                     new_post['time'],
-                     new_post['name'],
-                     new_post['email'],
-                     new_post['subject'],
-                     new_post['content'],
-                     new_post['filename'],
-                     new_post['spoiler']))
-    else:
-        cur.execute(sql,
-                    (post_id,
-                     new_post['board_id'],
-                     thread_id,
-                     new_post['op'],
-                     new_post['last_bump'],
-                     new_post['time'],
-                     new_post['name'],
-                     new_post['email'],
-                     new_post['subject'],
-                     new_post['content'],
-                     new_post['filename'],
-                     new_post['spoiler']))
+        cur.execute(sql, new_post)
     con.commit()
+    con.close()
+    return new_post
 
 def get_banner(board):
     banner = random.choice(os.listdir("static/banners"))
@@ -120,9 +102,11 @@ class PostForm(FlaskForm):
 def board_page(board):
     if board in boards:
         con = sqlite3.connect("tomochan.db")
+        con.row_factory = dict_factory
         cur = con.cursor()
-        threads = cur.execute("SELECT * FROM posts WHERE op = 1 AND board_id = '" + board + "' ORDER BY last_bump DESC")
-        threadlist = threads.fetchmany(size=15)
+        threads = cur.execute("SELECT * FROM posts WHERE op = 1 AND board_id = ? ORDER BY last_bump DESC", (board,))
+        threadlist = threads.fetchall()
+        con.close()
         print(threadlist)
 
         banner = get_banner(board)
@@ -133,30 +117,63 @@ def board_page(board):
                 name = form.name.data
             else:
                 name = "Anonymous"
-            email = form.email.data
-            subject = form.subject.data
-            spoiler = form.spoiler.data
             timestamp = last_bump = datetime.now(timezone.utc).timestamp()
             new_post = {'thread_id' : 'new',
                         'board_id' : board,
                         'op' : 1,
                         'last_bump' : last_bump,
                         'name' : name,
-                        'email' : email,
-                        'subject' : subject,
+                        'email' : form.email.data,
+                        'subject' : form.subject.data,
                         'content' : form.post.data,
-                        'spoiler' : spoiler,
+                        'spoiler' : form.spoiler.data,
                         'time' : timestamp,
                         'filename' : None}
-            post(new_post)
+            posted = post(new_post)
+            if form.email.data == "nonoko":
+                return redirect(url_for('board_page', board=board))
+            else:
+                return redirect(url_for('thread_page', board=posted['board_id'], thread=posted['thread_id']))
         return render_template('board.html', board=board, form=form, threads=threadlist, banner=banner)
     else:
         return send_file('static/404.html'), 404
 
-"""
+
 @app.route("/<board>/<thread>/", methods=['GET', 'POST'])
 def thread_page(board, thread):
     if board in boards:
-        thread = load_thread(board, thread)
-        if thread in
-"""
+        con = sqlite3.connect("tomochan.db")
+        con.row_factory = dict_factory
+        cur = con.cursor()
+        select_posts = cur.execute("SELECT * FROM posts WHERE thread_id = ?", (thread,))
+        posts = select_posts.fetchall()
+        con.close()
+        if not posts:
+            return send_file('static/404.html'), 404
+        else:
+            banner = get_banner(board)
+
+            form = PostForm()
+            if form.validate_on_submit():
+                if form.name.data:
+                    name = form.name.data
+                else:
+                    name = "Anonymous"
+                timestamp = datetime.now(timezone.utc).timestamp()
+                new_post = {'thread_id': thread,
+                            'board_id': board,
+                            'op': 0,
+                            'last_bump': None,
+                            'name': name,
+                            'email': form.email.data,
+                            'subject': form.subject.data,
+                            'content': form.post.data,
+                            'spoiler': form.spoiler.data,
+                            'time': timestamp,
+                            'filename': None}
+                posted = post(new_post)
+                return redirect(url_for('thread_page', board=board, thread=thread))
+
+            return render_template('thread.html', board=board, form=form, posts=posts, banner=banner)
+    else:
+        return send_file('static/404.html'), 404
