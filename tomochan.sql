b085172c2f1bc57c6eb8f9d7c53cd076e5e590b1
kait <keightbillion@gmail.com>
2025-04-13 16:32:43 -0400

    unfuck repo + db sql

---
+++ tomochan.sql
@@ -0,0 +1,17 @@
+CREATE TABLE posts (
+  post_id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+  board_id TEXT NOT NULL,
+  thread_id INTEGER NOT NULL,
+  op INTEGER NOT NULL,
+  last_bump INTEGER,
+  sticky INTEGER,
+  time INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  email TEXT,
+  subject TEXT,
+  content TEXT NOT NULL,
+  filename TEXT,
+  file_actual TEXT,
+  spoiler INTEGER NOT NULL,
+  ip TEXT NOT NULL
+);
