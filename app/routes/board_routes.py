ca5487e5b65ed0920a0be9d915ab668d25960efb
stophman1 <11683358+stophman1@users.noreply.github.com>
2025-04-18 14:07:31 -0500

    Update dict_factory()

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -64,10 +64,7 @@ sql = (
 
 
 def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+    return {col[0]: row[idx] for idx, col in enumerate(cursor.description)}
 
 
 @lru_cache(maxsize=1)
