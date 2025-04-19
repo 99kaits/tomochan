9061844cebd9a7e921d4aee253229537c0891e59
stophman1 <11683358+stophman1@users.noreply.github.com>
2025-04-19 12:40:32 -0500

    We got logback, but where is logforward?

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    alias(libs.plugins.kotlin.jvm)
 }
 
 group = "link.fraudulent"
@@ -7,4 +8,8 @@ version = "0.0.1"
 repositories {
     mavenCentral()
     maven { url = uri("https://packages.confluent.io/maven/") }
+}
+
+dependencies {
+    implementation(libs.logback.classic)
 }
