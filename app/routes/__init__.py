5bc0e4e09e38483fb7ddccbf97fcd84fccb6a5a4
stophman1 <11683358+stophman1@users.noreply.github.com>
2025-04-19 11:01:48 -0500

    Delete Python. Googbye

--- app/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from .upload_routes import upload_bp
-from .board_routes import board_bp
-from .other_routes import home_bp
-
-blueprints = [upload_bp, board_bp, home_bp]
